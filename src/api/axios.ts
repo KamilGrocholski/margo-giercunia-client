9a69ff3f8a49038d599e50d3dbf7392831e0ec05
Kazrek <mikololo26@interia.pl>
2022-09-10 23:56:04 +0200

    env problemy, jak zwykle

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,29 +1,19 @@
 import axios from 'axios'
 import { refresh } from './userAPI'
 
-let baseURL
-if (process.env.NODE_ENV === 'development') {
-    baseURL = 'http://localhost:5000/api/v1'
-} 
-if (process.env.NODE_ENV === 'production') {
-    baseURL = `${ process.env.REACT_APP_API_URL as string }/api/v1`
-} 
-else {
-    baseURL = 'http://localhost:5000/api/v1'
-}
-console.log(baseURL)
+const API_URL = process.env.NODE_ENV as string === 'production' ? `${ process.env.REACT_APP_API_URL as string }/api/v1` : 'http://localhost:5000/api/v1' 
 console.log(process.env.NODE_ENV)
-console.log(process.env.REACT_APP_API_URL)
+console.log(process.env.REACT_APP_API_URL as string)
 
 const instance = axios.create({
-    baseURL: baseURL,
+    baseURL: API_URL,
     headers: { 
         'Content-Type': 'application/json'
     }
 })
 
 export const axiosPrivate = axios.create({
-    baseURL: baseURL,
+    baseURL: API_URL,
     headers: { 'Content-Type': 'application/json' },
     withCredentials: true
 })
